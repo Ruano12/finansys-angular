42f3a56e39152d22482af8c46186b6979067a9fd
Ruano12 <mah.benjamim12@gmail.com>
2021-05-08 12:53:23 -0300

    Component server error e navbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,26 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterModule } from '@angular/router';
 
 import { HttpClientModule } from "@angular/common/http";
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from '../in-memory-database';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    RouterModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   exports:[
     //shared modules
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NavbarComponent
   ]
 })
 export class CoreModule { }

c94e8cc361d0c32e2863da92613a73feef81ff3f
Ruano12 <mah.benjamim12@gmail.com>
2021-05-11 20:10:46 -0300

    Chart implementado

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, Injector } from '@angular/core';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, flatMap } from 'rxjs/operators';
+import { catchError, flatMap, map } from 'rxjs/operators';
 
 import { Entry } from './entry.model';
 import { CategoryService } from '../../categories/shared/category.service';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
+
+import * as moment from "moment";
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +24,24 @@ export class EntryService extends BaseResourceService<Entry> {
 
   public update(entry: Entry): Observable<Entry> {
     return this.setCategoryAndSendToServer(entry, super.update.bind(this));  
+  }
+
+  public getByMonthAndYear(month: number, year: number): Observable<Entry[]>{
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries, month, year))
+    )
+  }
+
+
+  filterByMonthAndYear(entries: Entry[], month:number, year:number) {
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date, "DD/MM/YYYY");
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
+
+      if(monthMatches && yearMatches) return entry
+
+    })
   }
 
   private setCategoryAndSendToServer(entry: Entry, sendFn: any) : Observable<Entry>{
